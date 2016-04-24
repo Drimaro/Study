70069343d981205eff6856517dc67d9a77e45fea
Konstantin <Drimar@ya.ru>
2016-04-24 14:27:06 +0400

    java 8 examples

---
+++ src/ru/ksilin/study/java8/ChainedComparator.java
@@ -0,0 +1,87 @@
+package ru.ksilin.study.java8;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Created by Drimaro on 16.04.2016.
+ */
+public class ChainedComparator implements Comparator<ChainedComparator.CompareTest> {
+    @Override
+    public int compare(CompareTest o1, CompareTest o2) {
+
+        /*
+        Comparator<CompareTest> c = Comparator.comparing(o -> o.getId());
+        c = c.thenComparing(o -> o.getName());
+        return c.compare(o1, o2);
+        */
+
+        //return Comparator.comparing((CompareTest o) -> o.getId()).thenComparing(o -> o.getName()).compare(o1, o2);
+
+        return Comparator.comparing(CompareTest::getId)
+                .thenComparing(CompareTest::getName)
+                .compare(o1, o2);
+    }
+
+    public static void main(String[] args) {
+        int listSize = 3;
+        List<CompareTest> list = createList(listSize);
+
+        // Sort with chained comporator
+        ChainedComparator c = new ChainedComparator();
+        Collections.sort(list, c);
+        print(list);
+
+        list = createList(listSize);
+        // Sort using method mapping
+        list.sort(Comparator.comparing(CompareTest::getId));
+        print(list);
+
+        list = createList(listSize);
+        // Sort using lambda
+        list.sort((o1, o2) -> o1.getId() - o2.getId());
+        print(list);
+    }
+
+    private static void print(List<CompareTest> list) {
+        System.out.println("Print using function interface Consumer");
+        list.forEach((e) -> System.out.println(e.getId() + " " + e.getName()));
+
+        System.out.println("Print using method mapping. Requires method toString to be overriden");
+        list.forEach(System.out::println);
+    }
+
+    private static List<CompareTest> createList(int size){
+        Stream<CompareTest> stream = Stream.generate(() -> {
+            int i = (int) (Math.random()*10);
+            String name = "test" + i;
+            return new CompareTest(i, name);
+        });
+
+        return stream.limit(size).collect(Collectors.toList());
+    }
+
+    static class CompareTest {
+        private int id;
+        private String name;
+
+        CompareTest(int id, String name){
+            this.id = id;
+            this.name = name;
+        }
+
+        int getId() {
+            return id;
+        }
+
+        String getName() {
+            return name;
+        }
+
+        @Override
+        public String toString(){
+            return id + " " + name;
+        }
+    }
+}
---
+++ src/ru/ksilin/study/java8/MethodRef.java
@@ -0,0 +1,53 @@
+package ru.ksilin.study.java8;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
+/**
+ * Created by Drimaro on 23.04.2016.
+ */
+public class MethodRef {
+
+    public MethodRef(){
+        System.out.println("Some constructor text");
+    }
+
+    public static void main(String[] args) {
+        // Constructor
+        Supplier<MethodRef> s = MethodRef::new;
+        MethodRef methodRef = s.get();
+
+        // static
+        Consumer<String> c = MethodRef::staticPrint;
+        c.accept("Some static text");
+
+        // instance
+        c = methodRef::print;
+        c.accept("Some instance text");
+
+        //Consumer<String> c1 = MethodRef::print; // incorrect!!!
+        Consumer<MethodRef> c1 = MethodRef::print;
+        c1.accept(methodRef);
+
+
+    }
+
+    public void print(String text){
+        System.out.println(text);
+    }
+
+    public void print(){
+        System.out.println("Some instance in runtime");
+    }
+
+    public static void staticPrint(String text) {
+        System.out.println(text);
+    }
+
+    @Override
+    public String toString(){
+        return "test";
+    }
+}
