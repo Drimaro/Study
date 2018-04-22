3c45db5eb2b67304ac2cbc8e94a0d27a5ac81410
Konstantin <Drimar@ya.ru>
2018-04-22 23:49:51 +0400

    hackerrank warmups

---
+++ hackerrank/src/main/kotlin/ru/ksilin/study/hackerrank/warmup/MiniMaxSum.kt
@@ -0,0 +1,10 @@
+package ru.ksilin.study.hackerrank.warmup
+
+fun main(args: Array<String>) {
+    val arr = readLine()!!.split(" ").map{ it.trim().toLong() }.toLongArray()
+    arr.sort()
+
+    val minSum = arr.take(4).sum()
+    val maxSum = arr.takeLast(4).sum()
+    println("$minSum $maxSum")
+}
---
+++ hackerrank/src/main/kotlin/ru/ksilin/study/hackerrank/warmup/Staircase.kt
@@ -0,0 +1,19 @@
+package ru.ksilin.study.hackerrank.warmup
+
+import java.util.*
+
+fun staircase(n: Int) {
+    val stack = Stack<Char>()
+    for (i in 0 until n) {
+        repeat(n, { if (i >= it) stack.push('#') else stack.push(' ') })
+        while (!stack.isEmpty()) {
+            print(stack.pop())
+        }
+        println()
+    }
+}
+
+fun main(args: Array<String>) {
+    val n = readLine()!!.toInt()
+    staircase(n)
+}
