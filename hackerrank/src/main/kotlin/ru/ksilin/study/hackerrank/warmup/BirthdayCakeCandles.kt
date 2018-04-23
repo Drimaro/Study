9f95ffee148dbca07992f0a56c1a057cc0ae1180
Konstantin <Drimar@ya.ru>
2018-04-23 23:16:07 +0400

    hackerrank warmups

---
+++ hackerrank/src/main/kotlin/ru/ksilin/study/hackerrank/warmup/BirthdayCakeCandles.kt
@@ -0,0 +1,11 @@
+package ru.ksilin.study.hackerrank.warmup
+
+fun main(args: Array<String>) {
+    readLine()
+
+//    val countedMap = readLine()!!.split(" ").map{ it.trim().toInt() }.groupingBy { it }.eachCount()
+    val sortedList = readLine()!!.split(" ").map{ it.trim().toInt() }.sorted()
+
+    val amount = sortedList.takeLastWhile { sortedList.last() == it}.count()
+    println(amount)
+}
