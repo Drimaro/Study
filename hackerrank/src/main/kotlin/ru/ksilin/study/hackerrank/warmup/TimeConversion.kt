10f124ac5c77ca195bc7514a0342fa597ca291ae
Konstantin <Drimar@ya.ru>
2018-04-23 23:32:00 +0400

    hackerrank warmups

---
+++ hackerrank/src/main/kotlin/ru/ksilin/study/hackerrank/warmup/TimeConversion.kt
@@ -0,0 +1,11 @@
+package ru.ksilin.study.hackerrank.warmup
+
+import java.time.format.DateTimeFormatter
+
+fun main(args: Array<String>) {
+    val line = readLine()!!
+
+    val time = DateTimeFormatter.ofPattern("hh:mm:ssa").parse(line)
+
+    println(DateTimeFormatter.ISO_LOCAL_TIME.format(time))
+}
