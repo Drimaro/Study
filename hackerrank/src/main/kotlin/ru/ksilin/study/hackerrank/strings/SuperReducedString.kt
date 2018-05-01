cb1dcbb0aaf4d028b4c325a428bd4cfd33dac46e
Konstantin <Drimar@ya.ru>
2018-05-01 17:56:18 +0400

    hackerrank warmups refactor hackerrank strings

---
+++ hackerrank/src/main/kotlin/ru/ksilin/study/hackerrank/strings/SuperReducedString.kt
@@ -0,0 +1,36 @@
+package ru.ksilin.study.hackerrank.strings
+
+fun main(args: Array<String>) {
+    val toReduce = readLine()!!
+    var resultStr = toReduce
+    while(true) {
+        val (reduced, result) = reduce(resultStr)
+        resultStr = result
+        if (!reduced || resultStr.isBlank()) break
+    }
+    println(if (resultStr.isBlank()) "Empty String" else resultStr)
+}
+
+fun reduce(str: String) : ReduceResult {
+    var reduced = false
+    if (str.length < 2) return ReduceResult(reduced, str)
+
+    val sb = StringBuilder()
+    val toCharArray = str.toCharArray()
+    var i = 0
+    do {
+        val first = toCharArray[i]
+        val second = toCharArray[i+1]
+        if (first == second) {
+            i += 2
+            reduced = true
+        } else {
+            i++
+            sb.append(first)
+        }
+    } while (i < toCharArray.size-1)
+    if (i < toCharArray.size) sb.append(toCharArray.last())
+    return ReduceResult(reduced, sb.toString())
+}
+
+data class ReduceResult(val reduced: Boolean, val result: String)
