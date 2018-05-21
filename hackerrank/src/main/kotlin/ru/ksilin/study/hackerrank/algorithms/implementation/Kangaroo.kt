7c38cf499b3866a10731d779480c485716702dbe
Konstantin <Drimar@ya.ru>
2018-05-21 22:20:39 +0400

    hackerrank algorithms implementation without loop

--- hackerrank/src/main/kotlin/ru/ksilin/study/hackerrank/algorithms/implementation/Kangaroo.kt
+++ hackerrank/src/main/kotlin/ru/ksilin/study/hackerrank/algorithms/implementation/Kangaroo.kt
@@ -2,9 +2,9 @@ package ru.ksilin.study.hackerrank.algorithms.implementation
 
 fun main(args: Array<String>) {
     val x1V1X2V2 = readLine()!!.split(" ")
-    var x1 = x1V1X2V2[0].trim().toInt()
+    val x1 = x1V1X2V2[0].trim().toInt()
     val v1 = x1V1X2V2[1].trim().toInt()
-    var x2 = x1V1X2V2[2].trim().toInt()
+    val x2 = x1V1X2V2[2].trim().toInt()
     val v2 = x1V1X2V2[3].trim().toInt()
 
     if(v2 >= v1) println("NO")
