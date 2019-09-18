4253bd95510da128a0248665cf571e9a6f6af8c8
Konstantin <Drimar@ya.ru>
2019-09-18 15:14:58 +0300

    no dups in sorted array

--- sorting/src/main/kotlin/ru/ksilin/study/sorting/InsertionSort.kt
+++ sorting/src/main/kotlin/ru/ksilin/study/sorting/InsertionSort.kt
@@ -12,7 +12,30 @@ fun insertionSort(arr: IntArray) {
     }
 }
 
+/**
+ * remove duplicates from sorted array
+ */
+fun noDups(arr: IntArray): IntArray {
+    var elemId = 0
+    for (i in 1..arr.lastIndex) {
+        if (arr[elemId] == arr[i]) {
+            continue
+        } else {
+            arr[++elemId] = arr[i]
+        }
+    }
+    return arr.copyOfRange(0, elemId+1)
+}
+
 fun main() {
+//    insertionSortCheck()
+    val dupsArr = intArrayOf(1, 1, 2, 3, 3, 3, 4, 5, 6, 6, 6, 7, 7, 7, 7, 7)
+    val noDupsArr = noDups(dupsArr)
+    println("Array with duplicates ${dupsArr.contentToString()}")
+    println("Array without duplicates ${noDupsArr.contentToString()}")
+}
+
+private fun insertionSortCheck() {
     val arrToSort = Utils.getRandomIntArray(10)
     println(arrToSort.contentToString())
     insertionSort(arrToSort)
